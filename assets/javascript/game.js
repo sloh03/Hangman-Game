302c6a691250826d8087a2e3eb5cd363770ff70b
Stefanie Loh <stefanieloh@18AZCOPT030.local>
2018-02-11 15:45:10 -0700

    resets working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var words = ['MERCURY', 'JUPITER', 'ECLIPSE', 'GALAXY', 'ASTEROID', 'NEBULA', 'EUROPA', 'COMET']
+var words = ['MERCURY', 'JUPITER', 'ECLIPSE', 'GALAXY', 'ASTEROID', 'NEBULA', 'EUROPA', 'COMET', 'ASTRONAUT', 'SPACESHIP', 'PLANET', 'PLUTO', 'SUPERNOVA', 'ORBIT', 'SATURN', 'VENUS', 'EARTH', 'MARS', 'WORMHOLE', 'STAR' ]
 var currentWord;
 var currentLetters = [];
 
@@ -22,6 +22,12 @@ var losses = 0;
 
 // At start of game
 function startUp() {
+	// Clear arrays
+	answerArray = [];
+	newGuesses = []; 
+	incorrectGuesses = [];
+	correctGuesses = [];
+	correctGuessesDisplay = [];
 
 	// GENERATE WORD
 	// Choose a random word
@@ -29,7 +35,7 @@ function startUp() {
 
 	// Split current word into array of letters
 	currentLetters = currentWord.split("");
-	console.log(currentWord + " " + currentLetters);
+	console.log("New word: " + currentWord + " " + currentLetters);
 
 	// Display corresponding number of spaces
   	for (var i = 0; i < currentLetters.length; i++) {
@@ -40,10 +46,6 @@ function startUp() {
 	// Reset variables upon word generation
 	// Set amount of letters left to guess to word length
 	remainingLetters = currentLetters.length;
-	newGuesses = []; 
-	incorrectGuesses = [];
-	correctGuesses = [];
-	correctGuessesDisplay = [];
 	guessesRemaining = 15;
 	document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
 }
@@ -74,7 +76,7 @@ document.onkeyup = function(event) {
 			// Store letter in 'incorrectGuesses' array
 			incorrectGuesses.push(userGuess);
 			// Display in 'Letters Already Guessed' area on screen
-			document.getElementById("incorrect-guesses").innerHTML = incorrectGuesses;
+			document.getElementById("incorrect-guesses").innerHTML = incorrectGuesses.join(" ");
 		}
 		// If guess is a match
 		else {
@@ -90,144 +92,17 @@ document.onkeyup = function(event) {
 					document.getElementById("word-spaces").innerHTML = answerArray.join(" ");
 					console.log(remainingLetters);
 					console.log("Answer Array: " + answerArray);
-					if () {
-						
-					}
 				}
 			}
 		}
+	}
+	// RESTART ON WIN OR LOSS
+	if (remainingLetters === 0) {
+		wins++;
+		document.getElementById("wins").innerHTML = wins;
+		startUp();
+	}
+	if (guessesRemaining === 0) {
+		startUp();
 	}
 }
-
-
-/*
-
-			
-// COVERT SPACE TO LETTER
-
-// Attempt 1
-for (var i = 0; i < currentWord.length; i++) {
-				if (currentWord[i] === userGuess) {
-					answerArray[i] = userGuess;
-					document.getElementById("word-spaces").innerHTML = answerArray;
-					console.log("Answer Array: " + answerArray);
-				}
-			}
-
-
-// Attempt 2
-if (correctGuesses.length == 0) {
-	for (var i = 0; i < currentLetters.length; i++) {
-		correctGuessesDisplay[i] = "_";
-	}
-} 
-else {
-	for (var i = 0; i < currentLetters.length; i++) {
-		if (correctGuessesDisplay[i] != currentLetters[i]) {
-			for (var j = 0; j < correctGuesses.length; j++) {
-				if (correctGuesses[j] == currentLetters[i]) {
-					correctGuessesDisplay[i] == currentLetters[i];
-					document.getElementById("word-spaces").innerHTML = correctGuessesDisplay.join(" ");
-				}
-				else {
-					correctGuessesDisplay[i] = "_";
-				}
-			}	
-		}
-	}
-}
-
-
-		document.getElementById("wins").innerHTML = (wins);
-		for (var i=0; i < allGuesses.length; i++) {
-			
-		}
-
-		if (keysPressedTotal.indexOf(userGuessUpper) === -1) { //new letter
-			//Add to list of guessed letters
-			var newKeysPressed = [];
-			newKeysPressed.push(userGuessUpper);
-			console.log(newKeysPressed);
-
-	// Check if key pressed has been pressed before 
-	// If key has not been pressed before
-	if (keysPressedTotal.indexOf(userGuessUpper) === -1) {  
-		
-	}
-
-		// Take away 1 guesses remaining
-		guessesRemaining -= 1;
-		document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
-
-		// Check if key pressed is in word
-		// If key pressed is in word
-		if (currentWord.includes(userGuessUpper)) { // *** APPROPRIATE USE? ***
-			// Find the indices of the guess in the word
-			var a = [];// *** WHERE CAN I FIND CONTENTS OF THIS ARRAY? SHOWS EMPTY IN CONSOLE.LOG ***
-			function locations(substring, string) {
-				i=-1;
-				while((i=string.indexOf(substring, i+1)) >= 0) a.push(i);
-				return a;
-			}
-			//push to right array*, else push to wrong
-			var a = locations(userGuessUpper, currentWord);
-			// alert(a);
-
-
-			for (var i = a.length - 1; i >= 0; i--) {
-				currentWord[a[i]] = userGuessUpper;
-				alert(currentWord[a[i]]);
-			}
-
-
-			// Change corresponding '_' to key pressed
-			// Loop through the word and change the letter to letter guessed at the indices
-				/* var ? = '_ _ _ _' */ // *** WHAT VARIABLE IS HOLDING THE SPACES NOW? ***
-				/* ?.replaceAt(a, userGuessUpper) */ // *** DOES A HOLD THE INDEX NOW? CAN THIS BE SET TO A VARIABLE? ***
-				/* document.getElementById("word-spaces").innerHTML = ?; */ // REPLACE ? WITH VARIABLE HOLDING CHANGE.
-
-			// Place correct guesses in array
-/*			var matched_PrivateArray = []; // *** WHY ONLY STORING 1 LETTER AT A TIME? ***
-			matched_PrivateArray.push(userGuessUpper);
-			console.log(matched_PrivateArray);
-		}
-			
-		// Else -- key pressed not in word
-		else {
-			// Display letter in 'letters already guessed' area on screen after filtering duplicates and correct guesses
-			// Filter duplicates from original array	
-			var uniq = [];
-			var uniq = keysPressedTotal.reduce(function(a,b) { // *** WHY ONLY FILTERS IF FIRST LETTER PRESSED IS DUPLICATE? ***
-				if (a.indexOf(b) < 0 ) a.push(b);
-				return a;
-			},[]);
-			console.log(uniq, keysPressedTotal);
-			document.getElementById("incorrect-guesses").innerHTML = uniq; // Temporary testing, remove after filter out correct guesses
-
-			// Filter out correct guesses
-
-
-			/* document.getElementById("incorrect-guesses").innerHTML = incorrectGuessesDisplay; */
-		
-		
-	// When guesses remaining = 0
-
-		// Restart game
-
-	// When all letters in word are matched
-
-		// Add 1 to Wins
-
-		// Restart game		
-/*	} */
-
-
-
-
-
-
-
-
-
-	
-
